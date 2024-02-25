a6fd4be5ed24bd3207fd4aa793cec028293585bb
salah <salahdinjemal@gmail.com>
2024-02-25 10:16:27 -0600

    add project to rearrangement array

--- src/components/Preview/index.js
+++ src/components/Preview/index.js
@@ -7,23 +7,18 @@ import EducationPreview from './EducationPreview';
 import ExperiencePreview from './ExperiencePreview';
 import SkillsPreview from './SkillsPreview';
 import ProjectsPreview from './ProjectsPreview';
-import { dummyData } from "../../data";
 
-const Preview = () => {
-    const data = dummyData;
-    const personalInfo = data.personalInfo;
-    const education = data.education;
-    const experience = data.experience;
-    const skills = data.skills;
-    const projects = data.projects;
 
-    const [componentOrder, setComponentOrder] = useState(['Education', 'Experience', 'Skills']);
+const Preview = ({personalInfo, education = [], experience=[],skills=[], projects=[] }) => {
+
+    const [componentOrder, setComponentOrder] = useState(['Education', 'Experience', 'Skills', 'Projects']);
     const [isGeneratingPdf, setIsGeneratingPdf] = useState(false);
 
     const components = {
-        Education: <EducationPreview education={education} />,
-        Experience: <ExperiencePreview experience={experience} />,
-        Skills: <SkillsPreview skills={skills} />
+        Education: <EducationPreview education={Array.isArray(education) ? education : []} />,
+        Experience: <ExperiencePreview experience={Array.isArray(experience) ? experience : []} />,
+        Skills: <SkillsPreview skills={Array.isArray(skills) ? skills : []} />,
+        Projects : <ProjectsPreview projects={Array.isArray(projects) ? projects : []} />
     };
 
     const moveComponent = (component, direction) => {
@@ -84,7 +79,6 @@ const Preview = () => {
                     </div>
                 </div>
             ))}
-            <ProjectsPreview projects={projects} />
             {isGeneratingPdf ? null : <Button onClick={downloadPdf}>Download as PDF</Button>}
         </Box>
     );
