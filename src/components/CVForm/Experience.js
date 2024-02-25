fc1fe584abe745245bdf397b8ad76674a45da8ef
salah <salahdinjemal@gmail.com>
2024-02-25 10:15:29 -0600

    refactor

--- src/components/CVForm/Experience.js
+++ src/components/CVForm/Experience.js
@@ -1,55 +1,62 @@
-import { Stack, TextField, Button } from '@mui/material';
-import { useState, useEffect } from 'react';
-import { toCamelCase } from '../../util';
+import {Button, Stack, TextField} from '@mui/material';
+import {useEffect, useState} from 'react';
+import {toCamelCase} from '../../util';
 
 const fields = ['Company Name', 'Job Title', 'Start Year', 'End Year', 'Description'];
 
-const Experience = ({ experience, setExperience }) => {
-  const initialExperience = fields.reduce((obj, field) => ({ ...obj, [field]: '' }), {});
-  const [entries, setEntries] = useState(Array.isArray(experience) && experience.length > 0 ? experience : [initialExperience]);
+const Experience = ({experience, setExperience}) => {
+    const initialExperience = fields.reduce((obj, field) => ({...obj, [field]: ''}), {});
+    const [entries, setEntries] = useState(Array.isArray(experience) && experience.length > 0 ? experience : [initialExperience]);
 
-  const handleChange = (entryIndex, field) => (event) => {
-    const newEntries = [...entries];
-    newEntries[entryIndex][field] = event.target.value;
+    const handleChange = (entryIndex, field) => (event) => {
+        const newEntries = [...entries];
+        newEntries[entryIndex][field] = event.target.value;
 
 
-    setEntries(newEntries.map(entry => {
-        return Object.entries(entry).reduce((acc, [key, value]) => {
-            acc[toCamelCase(key)] = value;
-            return acc;
-        }, {});
-    }));
-  };
+        setEntries(newEntries.map(entry => {
+            return Object.entries(entry).reduce((acc, [key, value]) => {
+                acc[toCamelCase(key)] = value;
+                return acc;
+            }, {});
+        }));
+    };
 
-  const handleAddEntry = () => {
-    setEntries([...entries, initialExperience]);
-  };
+    const handleAddEntry = () => {
+        setEntries([...entries, initialExperience]);
+    };
 
-  const handleRemoveEntry = (entryIndex) => () => {
-    const newEntries = [...entries];
-    newEntries.splice(entryIndex, 1);
-    setEntries(newEntries);
-  };
+    const handleRemoveEntry = (entryIndex) => () => {
+        const newEntries = [...entries];
+        newEntries.splice(entryIndex, 1);
+        setEntries(newEntries);
+    };
 
-  useEffect(() => {
-    setExperience(entries);
-  }, [entries, setExperience]);
+    useEffect(() => {
+        setExperience(entries);
+    }, [entries, setExperience]);
 
-  return (
-    <>
-      {entries.map((entry, entryIndex) => (
-        <div key={entryIndex}>
-          {fields.map((field) => (
-            <Stack key={field} spacing={2}  direction="row" sx={{ marginBottom: 4 }}>
-              <TextField label={field} value={entry[field]} onChange={handleChange(entryIndex, field)}  fullWidth/>
-            </Stack>
-          ))}
-          <Button onClick={handleRemoveEntry(entryIndex)}>Remove</Button>
-        </div>
-      ))}
-      <Button onClick={handleAddEntry}>Add Entry</Button>
-    </>
-  );
+    return (
+        <>
+            {entries.map((entry, entryIndex) => (
+                <div key={entryIndex}>
+                    {fields.map((field) => (
+                        <Stack key={field} spacing={2} direction="row" sx={{marginBottom: 4}}>
+                            <TextField
+                                label={field}
+                                value={entry[field]}
+                                onChange={handleChange(entryIndex, field)}
+                                fullWidth
+                                multiline={field === 'Description'}
+                                rows={field === 'Description' ? 4 : 1}
+                            />
+                        </Stack>
+                    ))}
+                    <Button onClick={handleRemoveEntry(entryIndex)}>Remove</Button>
+                </div>
+            ))}
+            <Button onClick={handleAddEntry}>Add Entry</Button>
+        </>
+    );
 };
 
 export default Experience;
