980db80379a9ca1b2dd7dca3ab55a5ae0bcdcbe4
salah <salahdinjemal@gmail.com>
2024-02-25 19:41:03 -0600

    refactor

--- src/components/global/Sidebar.js
+++ src/components/global/Sidebar.js
@@ -1,18 +1,5 @@
 import {useState} from "react";
-import {
-    AppBar,
-    Box,
-    CssBaseline,
-    Divider,
-    Drawer,
-    IconButton,
-    List,
-    ListItem,
-    ListItemIcon,
-    ListItemText,
-    Toolbar,
-    Typography
-} from "@mui/material";
+import {Box, CssBaseline, Divider, Drawer, List, ListItem, ListItemIcon, ListItemText, Toolbar} from "@mui/material";
 import * as Icons from '@mui/icons-material';
 import {Link} from "react-router-dom";
 
@@ -26,24 +13,11 @@ const menuItems = [
     {title: "Sign Out", to: "/signout", icon: <Icons.Logout/>},
 ];
 
-const SidebarComponent = (props) => {
-    const {window} = props;
+const SidebarComponent = ({window}) => {
     const [mobileOpen, setMobileOpen] = useState(false);
-    const [isClosing, setIsClosing] = useState(false);
-
-    const handleDrawerClose = () => {
-        setIsClosing(true);
-        setMobileOpen(false);
-    };
-
-    const handleDrawerTransitionEnd = () => {
-        setIsClosing(false);
-    };
 
     const handleDrawerToggle = () => {
-        if (!isClosing) {
-            setMobileOpen(!mobileOpen);
-        }
+        setMobileOpen(!mobileOpen);
     };
 
     const drawer = (
@@ -52,14 +26,9 @@ const SidebarComponent = (props) => {
             <Divider/>
             <List>
                 {menuItems.map(({title, to, icon}) => (
-                    <ListItem key={title} disablePadding sx={{display: 'flex', flexDirection: 'row', padding: '10px'}}>
-                        <Link to={to} component="button"
-                              style={{textDecoration: 'none', color: 'inherit', display: 'flex', flexDirection: 'row'}}>
-                            <ListItemIcon>
-                                {icon}
-                            </ListItemIcon>
-                            <ListItemText primary={title}/>
-                        </Link>
+                    <ListItem button component={Link} to={to} key={title}>
+                        <ListItemIcon>{icon}</ListItemIcon>
+                        <ListItemText primary={title}/>
                     </ListItem>
                 ))}
             </List>
@@ -71,55 +40,20 @@ const SidebarComponent = (props) => {
     return (
         <Box sx={{display: 'flex'}}>
             <CssBaseline/>
-            <AppBar
-                position="fixed"
-                sx={{
-                    width: {sm: `calc(100% - ${drawerWidth}px)`},
-                    ml: {sm: `${drawerWidth}px`},
-                }}
-            >
-                <Toolbar>
-                    <IconButton
-                        color="inherit"
-                        aria-label="open drawer"
-                        edge="start"
-                        onClick={handleDrawerToggle}
-                        sx={{mr: 2, display: {sm: 'none'}}}
-                    >
-                        <Icons.Menu/>
-                    </IconButton>
-                    <Typography variant="h6" noWrap component="div">
-                        Responsive drawer
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-            <Box
-                component="nav"
-                sx={{width: {sm: drawerWidth}, flexShrink: {sm: 0}}}
-                aria-label="mailbox folders"
-            >
+            <Box component="nav" sx={{width: {sm: drawerWidth}, flexShrink: {sm: 0}}}>
                 <Drawer
                     container={container}
                     variant="temporary"
                     open={mobileOpen}
-                    onTransitionEnd={handleDrawerTransitionEnd}
-                    onClose={handleDrawerClose}
-                    ModalProps={{
-                        keepMounted: true,
-                    }}
-                    sx={{
-                        display: {xs: 'block', sm: 'none'},
-                        '& .MuiDrawer-paper': {boxSizing: 'border-box', width: drawerWidth},
-                    }}
+                    onClose={handleDrawerToggle}
+                    ModalProps={{keepMounted: true}}
+                    sx={{display: {xs: 'block', sm: 'none'}, '& .MuiDrawer-paper': {boxSizing: 'border-box', width: drawerWidth}}}
                 >
                     {drawer}
                 </Drawer>
                 <Drawer
                     variant="permanent"
-                    sx={{
-                        display: {xs: 'none', sm: 'block'},
-                        '& .MuiDrawer-paper': {boxSizing: 'border-box', width: drawerWidth},
-                    }}
+                    sx={{display: {xs: 'none', sm: 'block'}, '& .MuiDrawer-paper': {boxSizing: 'border-box', width: drawerWidth}}}
                     open
                 >
                     {drawer}
