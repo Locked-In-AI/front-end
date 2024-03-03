333f9034db377900137081c578e52e8245dc39fe
salah <salahdinjemal@gmail.com>
2024-03-02 22:08:50 -0600

    refactor

--- src/pages/CvListPage.js
+++ src/pages/CvListPage.js
@@ -1,27 +1,23 @@
 import React, {useEffect, useState} from 'react';
 import { Grid } from '@mui/material';
 import CvCard from "../components/CvCard";
-import { fetchCvs as cvs } from "../data";
-
+import { fetchCvs } from "../data";
 
 const CVList = () => {
     const [data, setData] = useState([]);
 
     useEffect(() => {
-        cvs().then(fetchedData => {
-            if (fetchedData) {
-                setData(fetchedData);
-            }
-        });
+        fetchCvs().then(setData);
     }, []);
 
-    return (<Grid container spacing={2}>
-        {data.map((cv, index) => (
-            <Grid item xs={12} sm={6} md={4} key={index}>
-                <CvCard cv={cv}/>
-            </Grid>
-        ))}
-    </Grid>
+    return (
+        <Grid container spacing={2}>
+            {data.map((cv, index) => (
+                <Grid item xs={12} sm={6} md={4} key={index}>
+                    <CvCard cv={cv}/>
+                </Grid>
+            ))}
+        </Grid>
     );
 };
 
