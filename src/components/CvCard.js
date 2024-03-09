f92cbf192ad0a9c2a3ed32da6179dc2fc43a8e30
salah <salahdinjemal@gmail.com>
2024-03-09 01:45:20 -0600

    add a button for editing cv

--- src/components/CvCard.js
+++ src/components/CvCard.js
@@ -2,32 +2,51 @@ import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import EditIcon from '@mui/icons-material/Edit';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import {useNavigate} from 'react-router-dom';
+import {formatDate} from '../utils/dateFormatter';
+import {Box} from "@mui/material";
 
 
 const CvCard = ({cv}) => {
-  return (
-    <Card sx={{ minWidth: 275 }}>
-      <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          {cv.created_at}
-        </Typography>
-        <Typography variant="h5" component="div">
-          {cv.title}
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          CV Description
-        </Typography>
-        <Typography variant="body2">
-          {cv.description}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">View CV</Button>
-      </CardActions>
-    </Card>
-  );
+    const navigate = useNavigate();
+
+    const handleViewCV = () => {
+        navigate('/build', {state: cv});
+    };
+    return (
+        <Card sx={{minWidth: 275}}>
+            <CardContent>
+                <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
+                    {formatDate(cv.created_at)}
+                </Typography>
+                <Typography variant="h5" component="div">
+                    {cv.title}
+                </Typography>
+                <Typography sx={{mb: 1.5}} color="text.secondary">
+                    CV Description
+                </Typography>
+                <Typography variant="body2">
+                    {cv.description}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Box display="flex" justifyContent="center">
+                    <IconButton onClick={() => {
+                        console.log("TODO: Implement edit CV functionality")
+                    }}>
+                        <EditIcon/>
+                    </IconButton>
+                    <IconButton onClick={handleViewCV}>
+                        <VisibilityIcon/>
+                    </IconButton>
+                </Box>
+            </CardActions>
+        </Card>
+    );
 }
 
 export default CvCard;
