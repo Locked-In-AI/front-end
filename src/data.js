245fb39fdadbe2f43443d80758909d04056497d8
salah <salahdinjemal@gmail.com>
2024-04-23 23:36:50 -0500

    refactor

--- src/data.js
+++ src/data.js
@@ -1,144 +1,67 @@
 import apiUrl from "./config";
-import {getAccessToken} from "./utils/auth";
-const dummyData = {
-    personalInfo: {
-        name: 'John Doe',
-        address: '123 Main St, Anytown, USA',
-        email: 'john.doe@example.com',
-        phone: '123-456-7890'
-    },
-    education: [
-        {
-            degree: 'Bachelor of Science in Computer Science',
-            schoolName: 'University of Anytown',
-            city: 'Anytown',
-            country: 'USA',
-            startYear: '2018',
-            endYear: '2022',
-            description: 'Focused on software development and data structures.'
-        }
-    ],
-    experience: [
-        {
-            jobTitle: 'Software Developer',
-            companyName: 'Tech Company',
-            startYear: '2022',
-            endYear: 'Present',
-            description: 'Working on developing and maintaining software applications.'
-        },
-        {
-            jobTitle: 'Intern',
-            companyName: 'Another Tech Company',
-            startYear: '2021',
-            endYear: '2022',
-            description: 'Assisted in the development of several software applications.'
-        }
-    ],
-    skills: [
-        {
-            skillName: 'JavaScript',
-            skillLevel: 'Expert'
-        },
-        {
-            skillName: 'React',
-            skillLevel: 'Intermediate'
-        }
-    ],
-    projects: [
-        {
-            projectName: 'Project 1',
-            description: 'This is a description of Project 1.',
-            startYear: '2020',
-            endYear: '2021',
-        },
-        {
-            projectName: 'Project 2',
-            description: 'This is a description of Project 2.',
-            startYear: '2020',
-            endYear: '2021',
-        }
-    ]
+import { getAccessToken } from "./utils/auth";
+
+const accessTokenPromise = getAccessToken(); // Fetch access token once
+
+async function fetchFromApi(endpoint, method = 'GET', body = null) {
+    const accessToken = await accessTokenPromise;
+
+    const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`
+    };
+
+    const options = {
+        method,
+        headers
+    };
+
+    if (body) {
+        options.body = JSON.stringify(body);
+    }
+
+    const response = await fetch(`${apiUrl}${endpoint}`, options);
+    if (!response.ok) {
+        throw new Error(`Failed to fetch data from ${endpoint}`);
+    }
+
+    return response.json();
+}
+
+const optimizeDescription = async (description) => {
+    const response = await fetchFromApi('/cv/enhance', 'POST', { description });
+
+    return response;
 };
 
-const dummyCVs = [
-    {
-        title: 'CV 1',
-        created_at: '2022-01-01',
-        description: 'This is a description for CV 1'
-    },
-    {
-        title: 'CV 2',
-        created_at: '2022-02-01',
-        description: 'This is a description for CV 2'
-    },
-    {
-        title: 'CV 3',
-        created_at: '2022-03-01',
-        description: 'This is a description for CV 3'
-    }
-];
+const fetchCv = async (id) => {
+    const data = await fetchFromApi(`/cv/${id}`);
 
+    return data;
+};
 
-async function fetchCvs() {
-    const accessToken = await getAccessToken();
+const fetchCvs = async () => {
+    const data = await fetchFromApi('/cv/');
 
-    const response = await fetch(`${apiUrl}/cv/`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${accessToken}`
-        }
-    });
-
-    const data = await response.json();
-    return response.ok ? data : null;
-}
+    return data;
+};
 
 const putCv = async (cv) => {
-    const accessToken = await getAccessToken();
+    const success = await fetchFromApi(`/cv/${cv.id}/`, 'PUT', cv);
 
-    const response = await fetch(`${apiUrl}/cv/${cv.id}/`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${accessToken}`
-        },
-        body: JSON.stringify(cv)
-    });
-
-    return response.ok;
-
-}
-
-const fetchCv = async (id) => {
-    const accessToken = await getAccessToken();
-
-    const response = await fetch(`${apiUrl}/cv/${id}/`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${accessToken}`
-        }
-    });
-
-    const data = await response.json();
-    return response.ok ? data : null;
-
-}
+    return success;
+};
 
 const fetchJobApplications = async () => {
-    const accessToken = await getAccessToken();
+    const data = await fetchFromApi('/application/job-application/');
 
-    const response = await fetch(`${apiUrl}/application/job-application/`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${accessToken}`
-        }
-    });
+    return data;
+};
 
-    const data = await response.json();
-    return response.ok ? data : null;
-}
+const pushJobApplications = async (jobApplication) => {
+    const data = await fetchFromApi('/application/job-application/', 'POST', jobApplication);
 
-export { dummyData, dummyCVs, fetchCvs, putCv, fetchJobApplications };
+    return data;
+};
+
+export { optimizeDescription, fetchCv, fetchCvs, putCv, fetchJobApplications, pushJobApplications };
